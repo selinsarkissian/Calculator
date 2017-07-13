5c8dc3fb829dd41de915043fe7aec525a3535d44
Student <Student@A2-PC>
2017-07-13 17:03:26 +0430

    after correcting fgsafrgafdgsdf

--- Calculator.UI/Program.cs
+++ Calculator.UI/Program.cs
@@ -17,6 +17,7 @@ namespace Calculator.UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            sdjfhgskd
         }
     }
 }
